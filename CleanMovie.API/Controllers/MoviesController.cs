d4a0313070677a9373aad40a837f16cde901bac8
Thành Nguyễn <yatogamitohka85@gmail.com>
2023-06-15 10:14:36 +0700

    Add project files.

---
+++ CleanMovie.API/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanMovie.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _service;
+
+        public MoviesController(IMovieService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Movie>> Get()
+        {
+            var moviesFromService = _service.GetAllMovies();
+            return Ok(moviesFromService);
+        }
+
+        [HttpPost]
+        public ActionResult<Movie> PostMovie(Movie movie)
+        {
+            var Movie = _service.CreateMovie(movie);
+            return Ok(Movie);
+        }
+    }
+}
